fd4846ea68955c214f80f932f9112dd906579755
rmrf <rmrf@localhost.localdomain>
2021-09-16 22:02:40 -0500

    :tada: Added service in kubernetes :kubernetes:

--- pulumisrc/__main__.py
+++ pulumisrc/__main__.py
@@ -4,8 +4,10 @@ import pulumi
 from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
 from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
 from pulumi_kubernetes.core.v1 import ContainerArgs, PodSpecArgs, PodTemplateSpecArgs
+from pulumi_kubernetes.core.v1 import Service
 
-app_labels = { "app": "nginx" }
+app_name = "nginx"
+app_labels = { "app": app_name }
 
 deployment = Deployment(
     "nginx",
@@ -18,4 +20,33 @@ deployment = Deployment(
         ),
     ))
 
+    
+config = pulumi.Config()
+is_minikube = config.require_bool("isMinikube")
+
+# Allocate an IP to the Deployment.
+frontend = Service(
+    app_name,
+    metadata={
+        "labels": deployment.spec["template"]["metadata"]["labels"],
+    },
+    spec={
+        "type": "ClusterIP" if is_minikube else "LoadBalancer",
+        "ports": [{ "port": 80, "target_port": 80, "protocol": "TCP" }],
+        "selector": app_labels,
+    })
+
+
+
+# When "done", this will print the public IP.
+result = None
+if is_minikube:
+    result = frontend.spec.apply(lambda v: v["cluster_ip"] if "cluster_ip" in v else None)
+else:
+    ingress = frontend.status.apply(lambda v: v["load_balancer"]["ingress"][0] if "load_balancer" in v else None)
+    if ingress is not None:
+        result = ingress.apply(lambda v: v["ip"] if "ip" in v else v["hostname"])
+
+
 pulumi.export("name", deployment.metadata["name"])
+pulumi.export("ip", result)
